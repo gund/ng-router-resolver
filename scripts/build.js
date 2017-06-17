bd3e60cf43f15a6473fa4509001042c947546d67
gund <malkevich.alex@gmail.com>
2017-06-18 01:08:23 +0400

    build(package): Add additional files to dist

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,4 @@
+const copyfiles = require('copyfiles');
 const package = require('../package.json');
 const { writeFileSync } = require('fs');
 
@@ -6,7 +7,15 @@ delete package.devDependencies;
 
 ['main', 'typings'].forEach(path => package[path] = fixDistPath(package[path]));
 
-writeFileSync('./dist/package.json', JSON.stringify(package, null, '  '));
+writeFileSync('dist/package.json', JSON.stringify(package, null, '  '));
+console.log('package.json was written to dist');
+
+copyfiles([
+  'LICENCE',
+  'README.md',
+  'dist' // Destination folder
+], {}, () => null);
+console.log('Additional files were copied to dist');
 
 function fixDistPath(path) {
   return path.replace('dist', '');
